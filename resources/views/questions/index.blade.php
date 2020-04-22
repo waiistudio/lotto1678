7b5e329bc3e650d2d61a933089d6428137293dc6
waiistudio <waiistudio@icloud.com>
2020-04-22 22:26:12 +0700

    add pagination
    add pagination

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -15,6 +15,21 @@
                 <div class="card-body">
                    @foreach ($questions as $question)
                         <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{ $question->votes }} </strong> {{ Str::plural('vote',$question->votes) }}
+                                </div>
+                            </div>
+                            <div class="d-flex flex-column counters">
+                                <div class="status">
+                                    <strong>{{ $question->answers }} </strong> {{ Str::plural('answer',$question->answers) }}
+                                </div>
+                            </div>
+                            <div class="d-flex flex-column counters">
+                                <div class="view">
+                                    {{ $question->views ." ". Str::plural('view',$question->views) }}
+                                </div>
+                            </div>
                             <div class="media-body">
                                 <h3 class="mt-0"><a href=" {{ $question->url }} ">{{ $question->title }}</a></h3>
                                 <p class="lead">
